8d8c88caf2520652f09deb6f978403f74bfea7aa
Huy (Sora) Trần Đức <huytran1998.dev@gmail.com>
2025-11-12 19:01:19 +0700

    + Initial 'WPF' project by Rider targeting .NET Standard 4.8 (Windows 7)

---
+++ Onsite.WPF/App.xaml.cs
@@ -0,0 +1,8 @@
+namespace Onsite.WPF {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ Onsite.WPF/MainWindow.xaml.cs
@@ -0,0 +1,10 @@
+namespace Onsite.WPF {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow {
+        public MainWindow() {
+            InitializeComponent();
+        }
+    }
+}
